0adfab667e3eec589895127faa8e2f7987200d6d
GitHub Action <action@github.com>
2021-11-22 19:00:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/randmaru/githubthemepack/services/MyProjectService.kt
+++ src/main/kotlin/com/github/randmaru/githubthemepack/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.randmaru.githubthemepack.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.randmaru.githubthemepack.MyBundle
 
 class MyProjectService(project: Project) {
 
